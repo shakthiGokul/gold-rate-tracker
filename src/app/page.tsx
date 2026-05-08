b65704ae6e75c1b6c7d305bd188e9db7475e85f7
Shakthi Gokul Panchavarnam <shakthigokulpanchavarnam@Shakthis-iMac.local>
2026-05-08 22:39:42 +0530

    goldRate-tracker

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+const HomePage = () => {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+    <p>Welcome To Gold Tracker</p>
+    </div>
+  );
+}
+
+export default HomePage
