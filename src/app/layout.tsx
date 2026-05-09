80be5dc2d0ecff3f3206acdfbe595191036b4a54
Shakthi Gokul Panchavarnam <shakthigokulpanchavarnam@Shakthis-iMac.local>
2026-05-10 00:09:24 +0530

    meta tage for mobile browser

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -20,7 +20,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" className={`${poppinsSans.variable}  h-full antialiased`}>
+    <html lang="en" className={`${poppinsSans.variable} h-full antialiased`}>
+      <head>
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+      </head>
       <body className="h-full flex flex-col">{children}</body>
     </html>
   );
